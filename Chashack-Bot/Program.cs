8d20a3ace20d0d74a0c39e0fa20f632e345d37c7
Johannes Flodin <jannis020609@gmail.com>
2026-03-03 20:33:42 +0100

    added apphost and fixed gitignore

--- Chashack-Bot/Program.cs
+++ Chashack-Bot/Program.cs
@@ -1,6 +1,34 @@
-using Chashack_Bot;
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+// Discord client configuration
+builder.Services.AddSingleton(provider =>
+{
+    var config = new DiscordSocketConfig
+    {
+        GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent,
+        AlwaysDownloadUsers = true,
+        LogLevel = LogSeverity.Info
+    };
+    return new DiscordSocketClient(config);
+});
+
+// Modals
+builder.Services.AddSingleton(provider =>
+{
+    var client = provider.GetRequiredService<DiscordSocketClient>();
+
+    var config = new InteractionServiceConfig
+    {
+        DefaultRunMode = RunMode.Async,
+        LogLevel = LogSeverity.Info
+    };
+
+    return new InteractionService(client, config);
+});
 
 var host = builder.Build();
 host.Run();
