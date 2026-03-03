768d546dc6f13dbbe4f1bfb9033d9efd69d62973
janne022 <jannis020609@gmail.com>
2026-03-03 19:48:05 +0100

    removed default template stuff

--- Chashack-Bot/Program.cs
+++ Chashack-Bot/Program.cs
@@ -1,7 +1,6 @@
 using Chashack_Bot;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
 host.Run();
--- Chashack-Bot/obj/Debug/net10.0/Chashack-Bot.AssemblyInfo.cs
+++ Chashack-Bot/obj/Debug/net10.0/Chashack-Bot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chashack-Bot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdb76f0eb6530f49f3c1e5acfdf500442ac43e91")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chashack-Bot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chashack-Bot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
